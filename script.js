48075e67a11c8d1f8a0963a39981eb081b45c18e
Aaron <trapp.root@gmail.com>
2024-07-31 10:50:56 -0700

    Added function to remove grid

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const tileContainer = document.querySelector(".container");
-let tileCount = 50;
+const resizeButton = document.querySelector(".resize");
+
+let tileLength = 16;
 
 const createTiles = () => {
-	const tileHeight = window.innerHeight / tileCount;
-	const tileWidth = window.innerWidth / tileCount;
-	for (let i = 1; i <= tileCount * tileCount; i++) {
+	const tileHeight = window.innerHeight / tileLength;
+	const tileWidth = window.innerWidth / tileLength;
+	for (let i = 1; i <= tileLength * tileLength; i++) {
 		const tile = document.createElement("div");
 		tile.classList.add("tile");
 		tile.style.width = `${tileWidth}px`;
@@ -15,8 +17,8 @@ const createTiles = () => {
 };
 
 const resizeTiles = () => {
-	const tileHeight = window.innerHeight / tileCount;
-	const tileWidth = window.innerWidth / tileCount;
+	const tileHeight = window.innerHeight / tileLength;
+	const tileWidth = window.innerWidth / tileLength;
 	const tiles = document.querySelectorAll(".tile");
 	tiles.forEach((tile) => {
 		tile.style.width = `${tileWidth}px`;
@@ -24,11 +26,27 @@ const resizeTiles = () => {
 	});
 };
 
-const alterTile = (e) => {
-	const tileStyle = e.target.style;
-	tileStyle.opacity -= "-0.1";
+const removeGrid = () => {
+	while (tileContainer.firstChild) {
+		tileContainer.removeChild(tileContainer.lastChild);
+	}
 };
 
-createTiles();
+const resizeGrid = () => {
+	// let newGridSize = prompt("What would you like to change grid length to?")
+	// if (typeof newGridSize !== "number" || (newGridSize < 1 || newGridSize > 100)) {
+	// 	resizeGrid();
+	// }
+	// tileLength = newGridSize
+	// createTiles();
+};
 
+const alterTile = (e) => {
+	const tileStyle = e.target.style;
+	tileStyle.opacity -= "-0.2";
+};
+
+resizeButton.addEventListener("click", removeGrid);
+
+window.onload = createTiles;
 window.onresize = resizeTiles;
